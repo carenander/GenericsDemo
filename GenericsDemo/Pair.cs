ad822dc8e671cb88a1d686bba886a8f7e678b398
DZM <d.bjorlingh@gmail.com>
2019-04-09 14:15:36 +0200

    Add project files.

---
+++ GenericsDemo/Pair.cs
@@ -0,0 +1,30 @@
+﻿namespace GenericsDemo
+{
+    internal class Pair<T> : Pair<T, T>
+    {
+
+        public Pair(T first, T second)
+            : base(first, second)
+        {
+        }
+    }
+
+    internal class Pair<T1, T2>
+    {
+        public T1 First { get; }
+        public T2 Second { get; }
+
+        public Pair(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override string ToString()
+        {
+            return $"[{First}, {Second}]";
+        }
+    }
+
+
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region kodexempel för max
+            /*
+            Console.WriteLine(Max(1, 2).GetType().Name);
+            Console.WriteLine(Max(1.2, 2.4).GetType().Name);
+            Console.WriteLine(Max(Math.Floor(-4.3f), Math.Floor(-1.2f)));
+            Console.WriteLine(Max("Anders", "Bengt"));
+            */
+            #endregion
+
+            var intPair = new Pair<int>(1, 2);
+            Console.WriteLine(intPair);
+
+            var stringPair = new Pair<string>("a", "b");
+            Console.WriteLine(stringPair);
+
+            var dualPair = new Pair<int, float>(1, 2.4f);
+            Console.WriteLine(dualPair);
+
+            var intStringPair = new Pair<int, string>(7, "seven");
+            Console.WriteLine(intStringPair);
+
+            var pairPair = new Pair<    Pair<int>,    Pair<string>   >(intPair, stringPair);
+            Console.WriteLine(pairPair);
+        }
+
+        private static T Max<T>(T a, T b) where T : IComparable
+        {       
+            if (a.CompareTo(b) > 0) return a;
+            return b;
+        }
+    }
+}
